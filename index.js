2133aa476932e125fe6d061ffac154013889627c
selimRU <selim.soha92@gmail.com>
2023-11-06 02:10:15 +0600

    available foods and details by id get api created as well as data loaded

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// pass: 2owku6biN0H5htv0
+// user: 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc6s3b6.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+    try {
+        await client.connect();
+        const featureFoodsCollections = client.db("foodSharingDB").collection("featureFoods")
+        const availableFoodCollections = client.db("foodSharingDB").collection("availableFoods")
+
+        // featured foods get api
+        app.get('/api/v1/featureFood', async (req, res) => {
+            const query = {}
+            console.log(query);
+            const result = await featureFoodsCollections.find(query).toArray()
+            res.send(result)
+        })
+
+        // available foods get api
+        app.get('/api/v1/availableFoods', async (req, res) => {
+            const query = {}
+            console.log(query);
+            const result = await availableFoodCollections.find(query).toArray()
+            res.send(result)
+        })
+        // available food by id
+        app.get('/api/v1/availableFoods/:id', async (req, res) => {
+            const id = req.params.id
+            const food = { _id: new ObjectId(id) }
+            const result = await availableFoodCollections.findOne(food)
+            res.send(result)
+        })
+
+        // foods post api
+        app.post('/api/v1/availableFoodsAdd', async (req, res) => {
+            const foods = req.body
+            const result = await availableFoodCollections.insertOne(foods)
+            console.log(result);
+            res.send(result)
+        })
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
