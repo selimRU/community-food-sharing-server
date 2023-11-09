56598ab3b8ef8c3fc7a56acef8611f4361515c01
selimRU <selim.soha92@gmail.com>
2023-11-09 19:18:52 +0600

    manage single food updated and displayed

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 require('dotenv').config()
+// const cookieParser = require('cookie-parser')
 const app = express()
+// const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173','https://community-food-sharing-70abe.web.app'],
+    credentials: true
+}))
 app.use(express.json())
 
 // pass: 2owku6biN0H5htv0
@@ -29,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
 
     try {
-        await client.connect();
+        // await client.connect();
         const featureFoodsCollections = client.db("foodSharingDB").collection("featureFoods")
         const availableFoodCollections = client.db("foodSharingDB").collection("availableFoods")
         const requestedFoodCollections = client.db("foodSharingDB").collection("requestedFood")
@@ -61,6 +66,15 @@ async function run() {
             res.send(result)
         })
 
+        // requested food delete
+        app.delete('/api/v1/requestedFoodDelete/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            // console.log(query);
+            const result = await requestedFoodCollections.deleteOne(query)
+            res.send(result)
+        })
+
         // available food by id
         app.get('/api/v1/availableFoods/:id', async (req, res) => {
             const id = req.params.id
@@ -75,7 +89,6 @@ async function run() {
             console.log('count', count);
             res.send({ count })
         })
-
         // available foods post api
         app.post('/api/v1/availableFoodsAdd', async (req, res) => {
             const foods = req.body
@@ -92,6 +105,33 @@ async function run() {
             // console.log(result);
             res.send(result)
         })
+
+        // update available food by donator
+        app.put('/api/v1/updateFoodByDonator/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const food = req.body;
+
+            const updatedFood = {
+                $set: {
+                    Food_Quantity: food.Food_Quantity,
+                    Donator_Image: food.Donator_Image,
+                    Food_Name: food.Food_Name,
+                    Food_Image: food.Food_Image,
+                    Donator_Email: food.Donator_Email,
+                    Pickup_Location: food.Pickup_Location,
+                    Expired_Date: food.Expired_Date,
+                    Additional_Notes: food.Additional_Notes,
+                    Food_Status: food.Food_Status
+                }
+
+            }
+            const result = await availableFoodCollections.updateOne(filter,
+                updatedFood)
+            console.log(result);
+            res.send(result)
+        })
+
         app.post('/api/v1/requestedFood', async (req, res) => {
             const foods = req.body
             const result = await requestedFoodCollections.insertOne(foods)
